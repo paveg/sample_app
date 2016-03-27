3a408716fbd5450e376b4d79008dd3a339d9ba2b
Ryota Ikezawa <ikezawa-ryota@c-fo.com>
2016-03-27 16:31:48 +0900

    The middle for sign-up-exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 
   test 'valid signup information' do
@@ -25,5 +27,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
